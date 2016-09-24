352a20634f8b1af49955b550dd550b6e2a2924f2
Armando Villanueva <Armando@Armandos-iMac.local>
2016-09-23 21:07:54 -0700

    Fixed button size on mobile

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 
 <h1>Inspire Me.</h1>
 <p>Use the button below to get inspired.</p>
-<button onClick="getQuote();">Get Inspired</button>
+<input type="button" onClick="getQuote();" value="Get Inspired"/>
 <div class="box">
   <blockquote>
     <p id="quote-text"></p>
